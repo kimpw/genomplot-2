7ac1147e3655a439b1dca49e676011ce5fd2770e
kimpw <55972474+kimpw@users.noreply.github.com>
2020-03-11 13:02:24 -0500

    Update local_specific_chr_gene_plot.R

--- R/local_specific_chr_gene_plot.R
+++ R/local_specific_chr_gene_plot.R
@@ -79,6 +79,7 @@ spec_chr_plot <- function(genedata,mapdata,tissuedata,chr=8,tag_p=3,y_lab="p") {
     geom_hline(aes(yintercept = 0), size = 3) +
     geom_hline(yintercept = 3, size = .5)
   
-  ggsave(filename="GenomPlot.tiff",plot = genom,device = "tiff",scale = 1,width = 20,height = 15,units = "in",
+  p<-ggsave(filename="GenomPlot.tiff",plot = genom,device = "tiff",scale = 1,width = 20,height = 15,units = "in",
   dpi = 300,limitsize = TRUE)
+  return(p)
 }
